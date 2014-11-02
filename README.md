69d5e64ece2c5f82e030b13551cec6896abd30ad
Pavel Kulbakin <p.kulbakin@gmail.com>
2014-11-02 10:42:34 +0300

    add action specific methods and basic documentation

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-beanstream
-==========
+Beanstream PHP API
+==================
 
-Beanstream PHP API wrapper
+Composer ready PHP wrapper for [Beanstream API](http://developer.beanstream.com/documentation/).
+
+## Installation
+
+The recommended way to install the library is using [Composer](https://getcomposer.org).
+
+```json
+{
+    "require": {
+        "propa/beanstream": "dev-master"
+    }
+}
+```
+
+## Handling Exceptions
+
+If server returns an unexpected response or error, PHP API throws *\Beanstream\Exception*.
+Positive error codes correspond to Beanstream API errors, see
+[Take Payment Errors](http://developer.beanstream.com/documentation/take-payments/errors/),
+[Analyze Payment Errors](http://developer.beanstream.com/documentation/analyze-payments/errors/),
+[Tokenize Payments Errors](http://developer.beanstream.com/documentation/tokenize-payments/errors/).
+Negative codes correspond to [cURL errors](http://curl.haxx.se/libcurl/c/libcurl-errors.html)
+(original cURL error codes are positive, in *\Beanstream\Exception* those are just reversed).
+Exception with zero error code are PHP API specific, e.g. *The curl extension is required* or
+*Unexpected response format*.
+
+Generally, any unsuccessful request, e.g. insufficient data or declined transaction, results in *\Beanstream\Exception*,
+thus *try..catch* is recommended for intercepting and handling them, see example below.
+
+## Your First Integration
+
+The sample below is an equivalent of original [example](http://developer.beanstream.com/documentation/your-first-integration/)
+from Beanstream.
+
+```php
+$bm = new \Beanstream\Messanger(YOUR_MERCHANT_ID, YOUR_API_PASSCODE);
+
+try {
+    $ts = $bm->payment(array(
+        'order_number' => '100001234',
+        'amount' => 100.00,
+        'payment_method' => 'card',
+        'card' => array(
+            'name' => 'John Doe',
+            'number' => '5100000010001004',
+            'expiry_month' => '02',
+            'expiry_year' => '17',
+            'cvd' => '123'
+        )
+    ));
+    
+    /*
+     * Handle successful transaction, payment method returns
+     * transaction details as result, so $ts contains that data
+     * in the form of associative array.
+     */
+} catch (\Beanstream\Exception $e) {
+    /*
+     * Handle transaction error, $e->code can be checked for a
+     * specific error, e.g. 211 corresponds to transaction being
+     * DECLINED, 314 - to missing or invalid payment information
+     * etc.
+     */
+}
+```
--- src/Beanstream/Messenger.php
+++ src/Beanstream/Messenger.php
@@ -35,13 +35,17 @@ class Messenger
     }
     
     /**
-     * Send request to a gateway
+     * Send request to a gateway.
+     * 
+     * This is a generic method, in most cases a specific
+     * one should be used, e.g. Messenger::createProfile()
      * 
      * @param string $action Gateway action (e.g. 'payments')
      * @param array[optional] $data Data to user with POST request, if not set GET request is used
+     * @param string[optional] $method HTTP method to use, by default is GET if there is no $data and PUT when there is $data set
      * @return array Decoded API response
      */
-    public function request($action, $data = null)
+    public function request($action, $data = null, $method = null)
     {
         if ( ! extension_loaded('curl')) {
             throw new Exception('The curl extension is required', 0);
@@ -55,25 +59,256 @@ class Messenger
         ));
         
         curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($req, CURLOPT_FAILONERROR, true);
         curl_setopt($req, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($req, CURLOPT_TIMEOUT, 30);
         
+        if (is_null($method)) {
+            if (is_null($data)) {
+                $method = 'GET';
+            } else {
+                $method = 'POST';
+            }
+        }
+        curl_setopt($req, CURLOPT_CUSTOMREQUEST, $method);
+        
         if ( ! is_null($data)) {
-            curl_setopt($req, CURLOPT_POST, true);
             curl_setopt($req, CURLOPT_POSTFIELDS, json_encode($data));
         }
         
-        $result = curl_exec($req);
-        if (false === $result) {
+        $raw = curl_exec($req);
+        if (false === $raw) {
             throw new Exception(curl_error($req), -curl_errno($req));
         }
         
-        $result = json_decode($result, true);
-        if (is_null($result)) {
+        $res = json_decode($raw, true);
+        if (is_null($res)) {
             throw new Exception('Unexpected response format', 0);
         }
         
-        return $result;
+        if (isset($res['code']) and 1 < $res['code']) {
+            throw new Exception($res['message'], $res['code']);
+        }
+        
+        return $res;
+    }
+    
+    /**
+     * Purchases and pre-authorization
+     * @link http://developer.beanstream.com/documentation/take-payments/purchases/card/
+     * @link http://developer.beanstream.com/documentation/legato/take-payment-legato-token/
+     * @link http://developer.beanstream.com/documentation/take-payments/purchases/cash/
+     * @link http://developer.beanstream.com/documentation/take-payments/purchases/cheque-purchases/
+     * 
+     * @param array Payment data, by default 'payment_method' is 'card'
+     * @return array Transaction details
+     */
+    public function payment($data)
+    {
+        $res = $this->request('payments', $data + array(
+            'payment_method' => 'card',
+        ), 'POST');
+        
+        return $res;
+    }
+    
+    /**
+     * Pre-authorization completion
+     * @link http://developer.beanstream.com/documentation/take-payments/pre-authorization-completion/
+     * 
+     * @param string $oid Transaction Id
+     * @param mixed $amount Order amount
+     * @param string[optional] $order_number
+     * @return array Transaction details
+     */
+    public function complete($tid, $amount, $order_number = null)
+    {
+        $data = array('amount' => $amount);
+        if ( ! is_null($order_number)) {
+            $data['order_number'] = $order_number;
+        }
+        $res = $this->request('payments/'.$tid.'/completions', $data, 'POST');
+        
+        return $res;
+    }
+    
+    /**
+     * Void (aka cancel)
+     * @link http://developer.beanstream.com/documentation/take-payments/voids/
+     * 
+     * @param string $tid Transaction Id
+     * @param mixed $amount Order amount
+     * @return array Transaction details
+     */
+    public function void($tid, $amount)
+    {
+        $res = $this->request('payments/'.$tid.'/void', array('amount' => $amount), 'POST');
+        
+        return $res;
+    }
+    
+    /**
+     * Return (aka refund)
+     * @link http://developer.beanstream.com/documentation/take-payments/return/
+     * 
+     * @param string $oid Transaction Id
+     * @param mixed $amount Order amount
+     * @param string[optional] $order_number
+     * @return array Transaction details
+     */
+    public function refund($tid, $amount, $order_number = null)
+    {
+        $data = array('amount' => $amount);
+        if ( ! is_null($order_number)) {
+            $data['order_number'] = $order_number;
+        }
+        $res = $this->request('payments/'.$tid.'/returns', $data, 'POST');
+        
+        return $res;
+    }
+    
+    /**
+     * Take payment - profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/take-payment-profiles/
+     * 
+     * @param string $pid Profile Id
+     * @param int $cid Card Id
+     * @param array $data Order data
+     * @param bool[optional] $complete Set to false for pre-auth
+     * @return array Transaction details
+     */
+    public function paymentProfile($pid, $cid, $data, $complete = true)
+    {
+        $res = $this->request('payments', $data + array(
+            'payment_method' => 'payment_profile',
+            'payment_profile' => array(
+                'complete' => $complete,
+                'customer_code' => $pid,
+                'card_id' => $cid,
+            )
+        ), 'POST');
+        
+        return $res;
+    }
+    
+    /**
+     * Create a new profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/create-new-profile/
+     * 
+     * @param array $data Profile data
+     * @return string Profile Id (aka customer_code)
+     */
+    public function createProfile($data)
+    {
+        $res = $this->request('profiles', $data, 'POST');
+        
+        return $res['customer_code'];
+    }
+    
+    /**
+     * Retrieve a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/retrieve-profile/
+     * 
+     * @param string $pid Profile Id
+     * @return array Profile data
+     */
+    public function retrieveProfile($pid)
+    {
+        $res = $this->request('profiles/'.$pid);
+        unset($res['code'], $res['message']);
+        
+        return $res;
+    }
+    
+    /**
+     * Update a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/update-profile/
+     * 
+     * @param string $pid Profile Id
+     * @param array $data Profile data
+     * @return TRUE
+     */
+    public function updateProfile($pid, $data)
+    {
+        $this->request('profiles/'.$pid, $data, 'PUT');
+        
+        return true;
+    }
+    
+    /**
+     * Delete a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/delete-profile/
+     * 
+     * @param string $id Profile Id
+     * @return TRUE
+     */
+    public function deleteProfile($pid)
+    {
+        $this->request('profiles/'.$pid, null, 'DELETE');
+        
+        return true;
+    }
+    
+    /**
+     * Add card to a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/add-card-profile/
+     * 
+     * @param string $pid Profile Id
+     * @param array $data Card data
+     * @return TRUE
+     */
+    public function addCard($pid, $data)
+    {
+        $this->request('profiles/'.$pid.'/cards', array('card' => $data), 'POST');
+        
+        /*
+         * XXX it would be more appropriate to return newly added card id,
+         * but Beanstream for some reason does not return it in result
+         */
+        return true;
+    }
+    
+    /**
+     * Retrieve cards in a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/retrieve-cards-profile/
+     * 
+     * @param string $pid Profile Id
+     * @return array Cards data
+     */
+    public function retrieveCards($pid)
+    {
+        $res = $this->request('profiles/'.$pid.'/cards');
+        
+        return $res['card'];
+    }
+    
+    /**
+     * Update card in a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/update-card-profile/
+     * 
+     * @param string $pid Profile Id
+     * @param int $cid Card Id
+     * @param array $data Card data
+     * @return TRUE
+     */
+    public function updateCard($pid, $cid, $data)
+    {
+        $this->request('profiles/'.$pid.'/cards/'.$cid, array('card' => $data), 'PUT');
+        
+        return true;
+    }
+    
+    /**
+     * Delete card from a profile
+     * @link http://developer.beanstream.com/documentation/tokenize-payments/delete-card-profile/
+     * 
+     * @param string $pid Profile Id
+     * @param int $cid Card Id
+     * @return TRUE
+     */
+    public function deleteCard($pid, $cid)
+    {
+        $this->request('profiles/'.$pid.'/cards/'.$cid, null, 'DELETE');
+        
+        return true;
     }
 }
